08be5ae2d7a65408124fca033a99d0dcf9e8e7de
Jennifer <cutejaneii@hotmail.com>
2018-02-13 15:08:04 +0800

    Update xls_to_jieba_dict.py

--- transformer/xls_to_jieba_dict.py
+++ transformer/xls_to_jieba_dict.py
@@ -1,6 +1,12 @@
 # coding=UTF-8
 import xlrd
 
+def get_speech(word):
+        speech = 'n'
+        if '形容' in word.encode('utf-8'):
+                speech = 'a'   # jieba 詞性'a'代表是形容詞
+        return speech
+
 dict_column={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
 
 output_filename='userdict.txt'
@@ -21,7 +27,8 @@ for input_file in input_files:
         for i in range(sheet.nrows-1):
                 value = sheet.cell_value(i+1, file_column)
                 if (len(value)>0):
-                        text_file.write(value.encode('utf-8')+' 1 n'+'\n')
+                        word_speech=get_speech(sheet.cell_value(i+1, dict_column['E']))
+                        text_file.write(value.encode('utf-8')+' 1 '+ word_speech +'\n')
                 print('processing '+ str(i) +'/' + str(sheet.nrows) + ':' + value)
 
 
