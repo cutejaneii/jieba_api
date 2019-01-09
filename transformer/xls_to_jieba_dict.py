d9421f5e111e86960a63dc9a11636a80fee5d63a
Jennifer <cutejaneii@hotmail.com>
2019-01-09 10:33:46 +0800

    Update xls_to_jieba_dict.py

--- transformer/xls_to_jieba_dict.py
+++ transformer/xls_to_jieba_dict.py
@@ -3,6 +3,8 @@ import xlrd
 
 def get_speech(word):
         speech = 'n'
+        if '人名' in word.encode('utf-8'):
+                speech = 'nr'   # jieba 詞性'a'代表是形容詞
         if '形容' in word.encode('utf-8'):
                 speech = 'a'   # jieba 詞性'a'代表是形容詞
         return speech
